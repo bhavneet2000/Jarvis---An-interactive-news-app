6fbbe8a16d95a71d3e620df43b0a3e07cedc05cf
Bhavneet Singh <bhavneetsingh2000@gmail.com>
2021-03-14 14:34:28 +0530

    Jarvis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState, useEffect } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
+import useStyles from './styles';
+import NewsCards from './components/NewsCards/NewsCards';
+import wordsToNumbers from 'words-to-numbers';
+const App = () => {
+    const alanKey = '059531821d8ed63c719275233d5a589b2e956eca572e1d8b807a3e2338fdd0dc/stage';
+    const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+    const classes = useStyles();
+    useEffect(() => { 
+        alanBtn({
+            key: alanKey,
+            
+            onCommand: ({ command, articles, number }) =>{
+                if(command === 'newHeadlines'){
+                    window.scrollTo(0, 0);
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+                }
+                else if(command==='highlight'){
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle+1);
+                }
+                else if(command==='open'){
+                    const parsedNumber = number.length > 2 ? wordsToNumbers(number, {fuzzy:true}):number
+                    const article = articles[parsedNumber-1];
+                    if(!article){
+                        alanBtn().playText('Please try that again');
+                    }
+                    else{
+                        alanBtn().playText('Opening..');
+                        window.open(article.url, '_blank');
+                    }
+                }
+            }
+        })
+    }, [])
+    
+    return (
+        <div>
+            <div className={classes.logoContainer}>
+                <img src="https://thumbs.gfycat.com/ForsakenImpoliteIaerismetalmark-size_restricted.gif" className={classes.jarvisLogo} alt="jarvis logo"/>
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/NewsCards/NewsCards.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Grid, Grow, Typography } from "@material-ui/core";
+import useStyles from './styles';
+import NewsCard from '../NewsCard/NewsCard';
+
+const infoCards = [
+    { color: '#ff3333', title: 'Latest News', text: 'Give me the latest news' },
+    { color: '#b30000', title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news' },
+    { color: '#ff6666', title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Narendra Modi...', text: 'What\'s up with PlayStation 5' },
+    { color: '#660000', title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from BBC News' },
+];
+
+const NewsCards = ({articles, activeArticle}) => {
+    const classes = useStyles();
+
+    if(!articles.length){
+        return (
+            <Grow in>
+                <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                    {infoCards.map((infoCard) => (
+                        <Grid item xs={12} sm={12} lg={6} md={6} style={{display: 'flex'}} className={classes.infoCard}>
+                            <div className={classes.card} style={{backgroundColor: infoCard.color}}>
+                                <Typography variant="h4" >{infoCard.title}</Typography>
+                                {
+                                    infoCard.info ? 
+                                    (<Typography variant="h6">
+                                        <strong>
+                                            {infoCard.title.split(' ')[2]}:</strong>
+                                            <br />
+                                            {infoCard.info}
+                                    </Typography>) 
+                                    : null
+                                }
+                                <Typography variant="h6">
+                                    Try Saying: <br />
+                                    <i>{infoCard.text}</i>
+                                    
+                                </Typography>
+
+                            </div>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grow>
+        );
+    }
+
+    return (
+        <Grow in>
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {articles.map((article, i) => (
+                    <Grid item xs={12} sm={12} lg={6} md={6} style={{display: 'flex'}}>
+                        <NewsCard article={article} activeArticle={activeArticle} i={i}/>
+                    </Grid>
+                ))}
+            </Grid>
+            
+        </Grow>
+    )
+}
+
+export default NewsCards;
